ce158ceea6b572ba0b7c3235a994f396bdaae31b
kanvull <minirorus2@gmail.com>
2024-02-29 12:38:35 +0400

    Create tests for .published scope

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_227_101_611) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_101611) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'product_categories', force: :cascade do |t|
-    t.string 'name', limit: 128, null: false
-    t.text 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['name'], name: 'index_product_categories_on_name', unique: true
+  create_table "product_categories", force: :cascade do |t|
+    t.string "name", limit: 128, null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_product_categories_on_name", unique: true
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'name', limit: 128, null: false
-    t.decimal 'price', precision: 7, scale: 2, null: false
-    t.integer 'quantity', default: 0, null: false
-    t.text 'description'
-    t.string 'image'
-    t.datetime 'published_at'
-    t.bigint 'product_category_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['name'], name: 'index_products_on_name', unique: true
-    t.index ['product_category_id'], name: 'index_products_on_product_category_id'
+  create_table "products", force: :cascade do |t|
+    t.string "name", limit: 128, null: false
+    t.decimal "price", precision: 7, scale: 2, null: false
+    t.integer "quantity", default: 0, null: false
+    t.text "description"
+    t.string "image"
+    t.datetime "published_at"
+    t.bigint "product_category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_products_on_name", unique: true
+    t.index ["product_category_id"], name: "index_products_on_product_category_id"
   end
 
-  add_foreign_key 'products', 'product_categories'
+  add_foreign_key "products", "product_categories"
 end
--- spec/factories/products.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :product do
-    name { "Sample Product" }
-    published_at { nil }
-  end
-end
