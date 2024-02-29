20cd8f9262e0b4e89d3e83eff9d7331df0976a94
kanvull <minirorus2@gmail.com>
2024-02-29 14:04:41 +0400

    Add more uniqueness to product and category names

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :random_product, class: Product do
-    name          { Faker::Commerce.product_name }
+    name          { "#{Faker::Commerce.product_name}_#{Faker::Number.number(digits: 5)}" }
     published_at  { Faker::Date.between(from: 7.days.ago, to: 2.days.from_now) }
     price         { Faker::Commerce.price(range: 1000..10000.0) }
     quantity      { Faker::Number.number(digits: 2) }
@@ -9,6 +9,6 @@ FactoryBot.define do
   end
   
   factory :random_product_category, class: ProductCategory do
-    name { Faker::Commerce.material }
+    name { "#{Faker::Commerce.material}_#{Faker::Number.number(digits: 5)}" }
   end
 end
