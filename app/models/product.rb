c3c6c87b10506b0e296fe7d95d2993b8bd93febb
kanvull <minirorus2@gmail.com>
2024-03-03 14:30:09 +0400

    Add pc scope for only published products cat show

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :product_category
 
   scope :published, -> { where("published_at <= ?", Time.zone.now) }
+  scope :in_category, ->(category_name) { where(product_category_id: ProductCategory.where(name: category_name)) }
   scope :where_name_is_like, ->(name) { where("name ILIKE ?", "%#{name}%") if name.present? }
   scope :min_price_gte, ->(min_price) { where("price >= ?", min_price) if min_price.present? }
   scope :max_price_lte, ->(max_price) { where("price <= ?", max_price) if max_price.present? }
--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,7 +1,5 @@
 class ProductCategory < ApplicationRecord
   has_many :products
 
-  def self.with_published_products
-    joins(:products).merge(Product.published).distinct
-  end
+  scope :with_published_products, -> { joins(:products).merge(Product.published).distinct }
 end
