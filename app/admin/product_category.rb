b941ea85a5473a499db5a5fbb2bfa72496de1042
kanvull <minirorus2@gmail.com>
2024-03-03 17:54:37 +0400

    Add admin page for product and category

--- app/admin/product_category.rb
+++ app/admin/product_category.rb
@@ -3,10 +3,9 @@ ActiveAdmin.register ProductCategory do
       selectable_column
       id_column
       column :name
-      column :created_at
       actions
     end
 
     # Specify the permitted parameters for the form
-    permit_params :name, :description
+    permit_params :name
   end
