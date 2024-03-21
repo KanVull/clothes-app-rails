b819d9e5784eccc57fab55d0d2ecde3b314ff8fa
kanvull <minirorus2@gmail.com>
2024-03-21 16:18:47 +0400

    Add simple show page for product

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/catalog(/:category_name)", to: "catalog#index", as: "catalog"
+  resources :products, only: %i[show]
 
   resource :cart, only: %i[show update]
   resources :orders
