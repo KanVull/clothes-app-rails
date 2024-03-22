348784db5d41802651bec0416850dc1ed1e25354
kanvull <minirorus2@gmail.com>
2024-03-22 15:08:28 +0400

    Add preview for orders_mailer

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -1,6 +1,7 @@
 class OrdersMailer < ApplicationMailer
   def order_created(order)
     @link = order_url(order)
+    @catalog_url = catalog_url
     mail(to: order.email, subject: "Store - Order created")
   end
 end
