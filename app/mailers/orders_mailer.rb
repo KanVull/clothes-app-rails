d55e60e1e98daac5bc737fe81fca7189201c59da
kanvull <minirorus2@gmail.com>
2024-03-22 18:11:39 +0400

    Fix mailer order_created to make right url to order

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -1,6 +1,6 @@
 class OrdersMailer < ApplicationMailer
   def order_created(order)
-    @link = order_url(order)
+    @link = order_by_uuid_url(uuid: order.uuid)
     @catalog_url = catalog_url
     mail(to: order.email, subject: "Store - Order created")
   end
