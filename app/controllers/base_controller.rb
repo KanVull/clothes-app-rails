f0eecef97010b24857875bd416ad12208b4dbe07
kanvull <minirorus2@gmail.com>
2024-04-02 16:56:13 +0400

    Add require_user! require_admin! methods, separate authentication and autorization

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,14 +1,8 @@
 class BaseController < ApplicationController
-  before_action :set_user
+  before_action :set_current_user
   before_action :set_cart
 
   private
-
-  def set_user
-    if cookies.signed["user_id"].present?
-      Current.user = User.find(cookies.signed[:user_id])
-    end
-  end
 
   def set_cart
     Current.cart = Cart.find_or_create_by(session_key: session.id.to_s)
