7389ed8c8bdd02f5fee292227e0cbcebfefe9e38
kanvull <minirorus2@gmail.com>
2024-04-05 12:07:42 +0400

    Fix new session bug, where user is not find - 404

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by!(email: params[:email])
+    user = User.find_by(email: params[:email])
 
     if user&.authenticate(params[:password])
       cookies.signed[:user_id] = {
