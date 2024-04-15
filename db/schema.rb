4dce7898e406ce42423991cabb770a6480ec2a4e
kanvull <minirorus2@gmail.com>
2024-04-15 17:41:37 +0400

    Add activation fields to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_093247) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_15_133859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -86,6 +86,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_09_093247) do
     t.boolean "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
   add_foreign_key "cart_products", "carts"
