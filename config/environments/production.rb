5082f218a45bb2345b24b2e36e8cc4aad4711dec
kanvull <minirorus2@gmail.com>
2024-04-18 21:15:27 +0400

    Change stmp port in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
-    port:                 587,
+    port:                 465,
     user_name:            Rails.application.credentials.dig(:smtp, :email),
     password:             Rails.application.credentials.dig(:smtp, :password),
     authentication:       "plain",
