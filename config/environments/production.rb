2c65cf64e5c235fb3a5e6254bbf0fa618de00ffd
kanvull <minirorus2@gmail.com>
2024-04-18 22:10:49 +0400

    Remove unused default url port from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: "store-lek8.onrender.com", port: 10000 }
+  config.action_mailer.default_url_options = { host: "store-lek8.onrender.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:         "smtp.gmail.com",
